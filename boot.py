204760309c2f59d32e2a4832985f2a3c3db58e98
turfptax <40579046+turfptax@users.noreply.github.com>
2022-08-30 08:53:44 -0500

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,7 @@
+#!micropython for the rp2040
+# Working conecept developed in micropython and planned to move to compiled code at a later date
+
+import ssd1306
+import machine
+from machine import Pin, I2C
+
