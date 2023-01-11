ac1b94c7b3db1c71f0824a0d501b8eea89e86903
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2023-01-10 21:44:29 -0600

    collectmuscle GUI
    Added GUI for collectmuscle.py

--- UDPserver/collectmuscle.py
+++ UDPserver/collectmuscle.py
@@ -7,7 +7,8 @@ import os
 import time
 import ast
 
-
+last_lask_packet = {'id': 'OM-LASK4', 'time': (2023, 1, 8, 22, 37, 28, 6, 8), 'data': [-591, -667, -747, -457], 'ticks': 53249, 'rec_time': 0.0001}
+last_band_packet = {'id': 'OM-Band12', 'time': (2023, 1, 8, 22, 37, 28, 6, 8), 'data': [4928, 4828, 4824, 4992, 4673, 4897, 4993, 4805, 5020, 6829, 4400, 5349], 'ticks': 356392, 'rec_time': 0.0001}
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -55,13 +56,38 @@ adc_dis = 65
 pop = 4
 buff = 25
 
-def draw_signal(data,t,screen=screen):
-    pg.draw.line(screen,(255,0,255),data['x'],data['y'],width=1)
+def draw_signal(x0,y0,x,y,position,screen=screen):
+    height = 40
+    max_samp = 6500
+    # Row Multiply by position # * pixel height
+    y0 = int((y0/max_samp)*40)+(position*40)
+    y = int((y/max_samp)*40)+(position*40)
+    # Left Padding
+    x += 50
+    x0 += 50
+    pg.draw.line(screen,(255,0,255),(x0,y0),(x,y),width=1)
 
-def get_packet(s=s):
-    data, address = s.recvfrom(4096)
-    text = data.decode('utf-8')
-    return(text)
+def draw_lask(packet,last_packet=last_lask_packet):
+    global count
+    global last_lask_packet
+    position = 0
+    if packet['rec_time']-last_packet['rec_time']>.1:
+        for i,y in enumerate(last_packet['data']):
+            draw_signal(count-1,y,count,packet['data'][i],position)
+            position += 1
+        last_lask_packet = packet
+    
+    
+
+def draw_band(packet,last_packet=last_band_packet):
+    global count
+    global last_band_packet
+    position = 4
+    if packet['rec_time']-last_packet['rec_time']>.1:
+        for i,y in enumerate(last_packet['data']):
+            draw_signal(count-1,y,count,packet['data'][i],position)
+            position += 1
+        last_band_packet = packet
     
 ### File Save Setup
 filenumber = len(os.listdir('../TrainingData/CSV/'))
@@ -71,30 +97,48 @@ file = open(f'../TrainingData/CSV/training_file{filenumber}.txt','w')
 t0 = time.time()
 delta_time = 0
 
+# Connection Variables
+lask_con = {'active':False,'seq':0}
+band_con = {'active':False,'seq':0}
+
+def get_packet(s=s,lask=lask_con,band=band_con):
+    packet = False
+    s.settimeout(1)
+    try:
+        data, address = s.recvfrom(4096)
+        text = data.decode('utf-8')
+        packet = ast.literal_eval(text)
+    except:
+        pass
+    return(packet)
+
 ######## Main Loop ########
+count = 0
 done = False
 while not done:
     for event in pg.event.get():
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_ESCAPE:
-                done = True
-                data_file.close()
-        elif event.type == pg.QUIT:
+        if event.type == pg.QUIT:
             data_file.close()
             done = True
-    packet = get_packet(s)
-    print('packet: ',packet)
-    pack = ast.literal_eval(packet)
-    pack['rec_time'] = time.time()-t0
-    file.write(str(pack) + '\n')
-    # Draw Screen
-    #
-    if 'OM-Band' in pack['id']:
-        device = 'OpenMuscle Band'
-    elif 'OM-LASK' in pack['id']:
-        device = 'OpenMuscle LASK'
+    pack = get_packet(s)
+    if pack:
+        pack['rec_time'] = time.time()-t0
+        file.write(str(pack) + '\n')
+        # Draw Screen
+        #
+        if 'OM-Band' in pack['id']:
+            draw_band(pack)
+            device = 'OpenMuscle Band'
+        elif 'OM-LASK' in pack['id']:
+            draw_lask(pack)
+            device = 'OpenMuscle LASK'
     pg.display.update()
+    count += 1
+    if count > 750:
+        screen.fill(0)
+        count = 0
     # End Draw Screen
+    
     
     
 data_file.close()
