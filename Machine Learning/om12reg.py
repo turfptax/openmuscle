02b43ef8d49d3f3a5e962ed37913bc738a6c507c
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2023-01-19 17:49:53 -0600

    ML start
    Machine Learning Phase Started

---
+++ Machine Learning/om12reg.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+print('hello world')
---
+++ Machine Learning/txt2csv.py
@@ -0,0 +1,76 @@
+# txt to csv
+# convert txt json data to csv for ml
+import csv
+
+
+
+lask = []
+band = []
+last_pair = None
+
+
+
+
+def send_chunk(data):
+    global lask
+    global band
+    temp = []
+    # array structure [signal_int * count,received time float]
+    if 'OM-LASK' in data['id']:
+        for i in data['data']:
+            temp.append(i)
+        temp.append(data['rec_time'])
+        lask.append(temp)
+    elif 'OM-Band' in data['id']:
+        for i in data['data']:
+            temp.append(i)
+        temp.append(data['rec_time'])
+        band.append(temp)
+
+def check_chunk():
+    global lask
+    global band
+    found = []
+    dindexL []
+    dindexB []
+    if len(band) > 10:
+        for i,x in enumerate(band):
+            time = x[-1]
+            for o,z in enumerate(lask):
+                if abs(z[-1] - time) < .02:
+                    found.append([x,z])
+                    last_pair = z[-1]
+    for i in dindexL:
+        del lask[i]
+    for i in dindexB:
+        del band[i]
+    if len(band) > 20:
+        band = band[-10:]
+    return found
+    
+
+
+text_file = open('datasetONE.txt','r')
+csv_file = open('datasetONE.csv','w')
+writer = csv.writer(csv_file)
+writer.writerow([1,2,3,4,5,6,7,8,9,10,11,12,'receive_time',1,2,3,4,'receive_time'])
+             
+matched_pairs = []                    
+for i in text_file.read().split('\n'):
+    #print(i)
+    j = eval(i)
+    send_chunk(j)
+    found = check_chunk()
+    for i in found:
+        writer.writerow(i[0] + i[1])
+
+csv_file.close()
+        
+    
+    
+
+
+    
+
+
+
