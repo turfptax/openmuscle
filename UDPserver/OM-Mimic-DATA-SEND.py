9e41de2e65291ebf676fac289f63c9e664ea308a
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2023-03-16 17:38:42 -0500

    MimicOMData

---
+++ UDPserver/OM-Mimic-DATA-SEND.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    return s.getsockname()[0]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+#packet["id"] = "OM-Band12"
+#packet["ticks"] = time.ticks_ms()
+#packet["time"] = time.localtime()
+#packet['data'] = data
+
+text_file = open('datasetTWO.txt','r')
+
+for i in text_file.read().split('\n'):
+    time.sleep(.009)
+    raw_data = i.encode('utf-8')
+    s.sendto(raw_data,('192.168.1.32',3145))
+    
+
+
+
+
